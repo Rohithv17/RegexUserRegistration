9b914a685b2d11f61b6ef5cbc7d8df6ef91a9dd1
Rohithv17 <148313542+Rohithv17@users.noreply.github.com>
2024-02-11 17:02:10 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
       UserRegistration u = new UserRegistration();
       String fn ="Rohith";   //User's first name
-        u.form(fn);
+        u.nameCheck(fn);
 
         }
     }
