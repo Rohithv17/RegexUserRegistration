0d3980b76e402975c412eede7ac6d838c17483f2
Rohithv17 <148313542+Rohithv17@users.noreply.github.com>
2024-02-11 17:01:43 +0530

    Update UserRegistration.java

--- src/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
+++ src/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.UserRegistrationProblem;
 import java.util.regex.*;
 public class UserRegistration {
-    public void form(String fn) {
-        Pattern p1 = Pattern.compile("[A-Z][a-zA-Z]{3,}");  //First letter starts with capital and second letter can start with any alphabet(case insensitive) and first name must contain atleast 3 characters
+    public void nameCheck(String fn) {
+        Pattern p1 = Pattern.compile("[A-Z][a-zA-Z]{3,}");  //In first name first letter starts with capital and second letter can start with any alphabet(case insensitive) and first name must contain atleast 3 characters
         Matcher m1 = p1.matcher(fn);
 
         if (m1.matches()) {
