444fc4ebd25ddbd74ae434b92f676b0bc19ed5dd
Rohithv17 <rohithv1396@gmail.com>
2024-02-11 16:53:10 +0530

    Added feature to validate first name

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ import com.bridgelabz.UserRegistrationProblem.UserRegistration;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+      UserRegistration u = new UserRegistration();
+      String fn ="Rohith";   //User's first name
+        u.form(fn);
 
         }
     }
--- src/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
+++ src/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
@@ -1,6 +1,16 @@
 package com.bridgelabz.UserRegistrationProblem;
 import java.util.regex.*;
 public class UserRegistration {
+    public void form(String fn) {
+        Pattern p1 = Pattern.compile("[A-Z][a-zA-Z]{3,}");  //First letter starts with capital and second letter can start with any alphabet(case insensitive) and first name must contain atleast 3 characters
+        Matcher m1 = p1.matcher(fn);
 
-    }
+        if (m1.matches()) {
+            System.out.println("It is a valid first name");  //To check for validity of first name
+        } else {
+            System.out.println("Invalid first name!");
+        }
+
+
+}}
 
